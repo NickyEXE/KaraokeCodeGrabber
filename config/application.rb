f79c35508b1856eebb913c57d7cb550ee1532f8c
NickyEXE <dovern42@gmail.com>
2019-06-25 11:18:39 -0400

    hashing back in the rspotify authentication after setting env variables on heroku

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module KaraokeCable
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    # RSpotify.authenticate(ENV["CLIENTID"], ENV["CLIENTSECRET"])
+    RSpotify.authenticate(ENV["CLIENTID"], ENV["CLIENTSECRET"])
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
