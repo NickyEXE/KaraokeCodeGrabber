0364888c6086f438389362f03a038ea6b3e28846
NickyEXE <dovern42@gmail.com>
2019-08-19 15:18:39 -0400

    another update

--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -1,4 +1,5 @@
 class SongSerializer < ActiveModel::Serializer
-  attributes :id, :spotify_artist, :spotify_name, :code, :title, :artist, :lyrics
+  attributes :id, :spotify_artist, :spotify_name, :code, :title, :artist, :album_name, :album_art, :release_date, :artist_url, :url, :lyrics
+
 
 end
