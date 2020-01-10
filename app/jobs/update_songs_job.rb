64b114248c6e9997e8b605e15bd07d2c0ae05fc0
NickyEXE <dovern42@gmail.com>
2020-01-10 18:48:59 -0500

    more puts statements baby

--- app/jobs/update_songs_job.rb
+++ app/jobs/update_songs_job.rb
@@ -1,10 +1,20 @@
 class UpdateSongsJob < ApplicationJob
   queue_as :default
 
+  around_perform :around_cleanup
+
   def perform(playlist_id)
     puts "why hello there"
     puts "we're performing the update songs jobs!"
     puts(playlist_id)
     # Playlist.find(playlist_id).songs.filter{|song| !song.code}.each{|song| song.fetch_number}
   end
+
+  private
+  def around_cleanup
+    puts "I'm about to perform a job!"
+    yield
+    puts "I just performed a job!"
+  end
+
 end
