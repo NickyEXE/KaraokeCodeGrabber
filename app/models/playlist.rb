6c0d784d91b4368d082da82ca2d4e47ebcae9645
NickyEXE <dovern42@gmail.com>
2020-02-19 15:14:35 -0500

    made it so that it pulls all the playlist's songs

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,13 +1,6 @@
 class Playlist < ApplicationRecord
     has_and_belongs_to_many :songs
 
-
-    
-    def self.fetch_playlist(url)
-        uri = url.split("/")[url.split("/").index("playlist")+1][0..21]
-        self.get_playlist_from_spotify_uri(uri)
-    end
-
     def self.get_playlist_from_spotify_uri(uri)
         client_token = Base64.strict_encode64(ENV["SPOTIFYCLIENTID"] + ":" + ENV["SPOTIFYCLIENTSECRET"])
         spotify_token = RestClient.post("https://accounts.spotify.com/api/token",{"grant_type": "client_credentials"}, {"Authorization": "Basic #{client_token}"})
@@ -16,9 +9,10 @@ class Playlist < ApplicationRecord
     end
 
     def self.create_playlist_by_spotify_link(url, creator)
-        spotify_data = Playlist.fetch_playlist(url)
+        uri = url.split("/")[url.split("/").index("playlist")+1][0..21]
+        spotify_data = Playlist.get_playlist_from_spotify_uri(uri)
         if spotify_data
-            add_playlist_by_spotify_data_and_creator(spotify_data, creator)
+            add_playlist_by_spotify_data_and_creator(spotify_data, creator, uri)
         else
             puts "No Spotify Data available."
         end
@@ -26,25 +20,42 @@ class Playlist < ApplicationRecord
 
     def self.create_playlist_by_uri(uri)
         spotify_data = self.get_playlist_from_spotify_uri(uri)
-        self.add_playlist_by_spotify_data_and_creator(spotify_data, "THIS_IS_THE_NULL_CASE")
+        self.add_playlist_by_spotify_data_and_creator(spotify_data, "THIS_IS_THE_NULL_CASE", uri)
     end
 
-    def self.add_playlist_by_spotify_data_and_creator(spotify_data, creator)
+    def self.add_playlist_by_spotify_data_and_creator(spotify_data, creator, uri)
         creator == "THIS_IS_THE_NULL_CASE" && creator = spotify_data["owner"]["display_name"]
         playlist = Playlist.create(name: spotify_data["name"], description: CGI::unescapeHTML(spotify_data["description"]), creator: creator, image_url: spotify_data["images"][0]["url"])
             # Adding the songs to playlists, then getting the code, because the scrape is slow and I want the users to be able to use the playlists beforehand.
         playlist.add_all_songs_to_playlist(spotify_data["tracks"]["items"])
-            # making this happen after the original results are rendered
-            # playlist.get_codes
-        playlist
+            # Spotify only pulls 100 tracks, but allows you to make additional offsetted fetch for the rest of your songs
+        total_number_of_tracks = spotify_data["tracks"]["total"]
+        total_number_of_tracks > 100 && playlist.grab_additional_tracks(uri, total_number_of_tracks)
     end
-
 
     def add_all_songs_to_playlist(spotify_tracks)
         spotify_tracks.each do |track|
             self.first_or_create_checking_for_albums(track["track"])
         end
     end
+
+    def grab_additional_tracks(uri, total_number_of_tracks)
+        client_token = Base64.strict_encode64(ENV["SPOTIFYCLIENTID"] + ":" + ENV["SPOTIFYCLIENTSECRET"])
+        spotify_token = RestClient.post("https://accounts.spotify.com/api/token",{"grant_type": "client_credentials"}, {"Authorization": "Basic #{client_token}"})
+        parsed_token = JSON.parse(spotify_token)
+        offset = 100
+        while offset < total_number_of_tracks
+            self.make_offsetted_fetch(uri, offset, parsed_token)
+            offset += 100
+        end
+    end
+
+    def make_offsetted_fetch(uri, offset, parsed_token)
+        spotify_tracks = JSON.parse(`curl -X "GET" "https://api.spotify.com/v1/playlists/#{uri}/tracks?offset=#{offset}" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer #{parsed_token["access_token"]}"`)["items"]
+        self.add_all_songs_to_playlist(spotify_tracks)
+    end
+
+
 
     def first_or_create_checking_for_albums(track)
         song = Song.where(spotify_name: track["name"], spotify_artist: track["artists"].first["name"]).first
